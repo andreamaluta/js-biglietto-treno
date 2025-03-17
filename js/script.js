0b089caba3454a469ac5c117ad5aa95d4d156821
andreamaluta <andreamaluta@gmail.com>
2025-03-17 14:20:10 +0100

    [UPD] made the request of km and the age

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+// chiedo di inserire la distanza desiderata
+let chilometri = parseFloat(prompt("Inserisci quanti chilometri desideri percorrere"));
+console.log(chilometri)
+// chiedo di inserire l'età
+let anni = parseInt(prompt("Inserisci quanti anni hai"));
+console.log(anni)
+
